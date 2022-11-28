912d262b8f851142610186f83cf710528f1048e9
GianlucaPtrn <200188@site.asty-moulin.be>
2022-11-28 11:58:01 +0100

    add files uncompleted

---
+++ Act4_ExerciceSimple_Personne/Act4_ExerciceSimple_Personne/Personne.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Act4_ExerciceSimple_Personne
+{
+    class Personne
+    {
+        private string _nom;
+        private double _euro;
+
+        public string Nom
+        {
+            get{
+                return _nom; 
+            }
+            set{
+                _nom = value; 
+            }
+        }
+
+        public double Euro
+        {
+            get{
+                return _euro;
+            }
+            set{
+                _euro = value;
+            }
+        }
+
+        public Personne(string nom, double euro){
+            _nom = nom;
+            _euro = euro;
+        }
+
+        public double GiveMoney(Personne[] personnes ){
+            Console.WriteLine(personnes[0].Nom + " combien Voulez vous donner à " + personnes[1].Nom);
+            Console.WriteLine("Encodez une somme d'argent !!");
+            double money = double.Parse(Console.ReadLine());
+            double giveMoney = _euro - money;
+            return giveMoney;
+        }
+
+        public double ReceiveMoney(double giveMoney)
+        {
+            double receiveMoney = _euro + giveMoney;
+            Console.WriteLine("Ajout effectué !!");
+            return receiveMoney;
+        }
+
+        public string sentenceMoney(Personne[] personnes, double giveMoney, double receiveMoney)
+        {
+            string sentence = "";
+            for(int i = 0; i < 2; i++)
+            {
+                sentence = personnes[i].Nom + " a " + _euro + " euro dans son porte-feuille";
+            }
+            return sentence;
+        }
+    }
+}
---
+++ Act4_ExerciceSimple_Personne/Act4_ExerciceSimple_Personne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Act4_ExerciceSimple_Personne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenu dans notre gestionnaire de porte monaie");
+            Personne[] personnes = new Personne[2];
+            personnes[0] = new Personne("volt", 0);
+            personnes[1] = new Personne("Rox", 0);
+
+            Console.WriteLine("Permière personne qu'elle est votre nom ?");
+            personnes[0].Nom = Console.ReadLine();
+            Console.WriteLine("Encodez une somme d'argent !!");
+            personnes[0].Euro = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Permière personne qu'elle est votre nom ?");
+            personnes[1].Nom = Console.ReadLine();
+            Console.WriteLine("Encodez une somme d'argent !!");
+            personnes[1].Euro = int.Parse(Console.ReadLine());
+
+
+            personnes[0] = new Personne(personnes[0].Nom, personnes[0].Euro);
+            personnes[1] = new Personne(personnes[1].Nom, personnes[1].Euro);
+
+            Console.WriteLine(personnes[0].sentenceMoney());
+        }
+    }
+}
+    
